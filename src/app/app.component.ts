91744674170a096442da961cb29c582c162b4dae
Peter Gray <peter@petergraycreative.com>
2018-06-06 11:05:02 -0500

    Began working on rest API calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,32 @@ import { map } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Listings App';
+  restItems: any;
+  restItemsUrl = 'https://public-api.wordpress.com/rest/v1.1/sites/vocon-it.com/posts';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRestItems();
+  }
+
+  // Read all REST Items
+  getRestItems(): void {
+    this.restItemsServiceGetRestItems()
+      .subscribe(
+        restItems => {
+          this.restItems = restItems;
+          console.log(this.restItems);
+        }
+      )
+  }
+
+  // Rest Items Service: Read all REST Items
+  restItemsServiceGetRestItems() {
+    return this.http
+      .get<any[]>(this.restItemsUrl)
+      .pipe(map(data => data));
+  }
 }
